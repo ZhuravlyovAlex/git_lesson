c9ed6221f13bef190c46d20306b6dc1a4b6b1cb5
Alexey <ZhuravlyovAlex@mail.ru>
2020-10-31 07:49:22 +0300

    lesson #4

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -3,10 +3,35 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.print("Введите ваше имя: ");
-        char symbol = in.next().charAt(3);
-        System.out.println(symbol);
+        byte num = 15;
+        switch(num) {
+            case 1:
+                System.out.println("один");
+                break;
+            case 15:
+                System.out.println("пятнадцать");
+                break;
+            default:
+                System.out.println("всякая фигня");
+        }
+
+        short x = 15, a = 12;
+         int res = x < a ? (x + a) : (x - a);
+        System.out.println(res);
+
+        System.out.println("Введите свой возраст: ");
+       Scanner in = new Scanner(System.in);
+       int age = in.nextInt();
+       if(age < 18){
+           System.out.println("Вы еще слишком малы");
+       }else if(age == 18){
+           System.out.println("Вам уже 18");
+       }else{
+           if(age < 50)
+                System.out.println("Вам меньше 50 лет");
+           else
+               System.out.println("Вам больше 50 лет");
+       }
 
     }
 }
